4e4afedfa1bb42532e8425d4656118dc2ff69727
Brian Conway <bconway@rcesoftware.com>
2021-06-03 21:07:46 -0500

    Add gateway connection state support to atlas-lora-ingestor
    - Sync `vendor/` for unused dependencies.
    - All test variations pass.

--- internal/atlas-lora-ingestor/test/ingestor_test.go
+++ internal/atlas-lora-ingestor/test/ingestor_test.go
@@ -31,47 +31,112 @@ func TestDecodeGateways(t *testing.T) {
 		inpProto proto.Message
 		res      []*message.ValidatorIn
 	}{
-		{"lora/gateway/" + uniqID + "/event/up", &gw.UplinkFrame{
-			RxInfo: &gw.UplinkRXInfo{Rssi: -74}}, []*message.ValidatorIn{
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "raw_gateway",
-				ValOneof: &common.DataPoint_StrVal{
-					StrVal: `{"rxInfo":{"rssi":-74}}`}}, SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "lora_rssi",
-				ValOneof: &common.DataPoint_IntVal{IntVal: -74}},
-				SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "channel",
-				ValOneof: &common.DataPoint_IntVal{IntVal: 0}},
-				SkipToken: true},
-		}},
-		{"lora/gateway/" + uniqID + "/event/stats", &gw.GatewayStats{
-			RxPacketsReceivedOk: 2}, []*message.ValidatorIn{
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "raw_gateway",
-				ValOneof: &common.DataPoint_StrVal{
-					StrVal: `{"rxPacketsReceivedOK":2}`}}, SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "rx_received_valid",
-				ValOneof: &common.DataPoint_IntVal{IntVal: 2}},
-				SkipToken: true},
-		}},
-		{"lora/gateway/" + uniqID + "/event/ack", &gw.DownlinkTXAck{
-			Items: []*gw.DownlinkTXAckItem{{Status: gw.TxAckStatus_OK}}},
-			[]*message.ValidatorIn{
-				{Point: &common.DataPoint{UniqId: uniqID, Attr: "raw_gateway",
-					ValOneof: &common.DataPoint_StrVal{StrVal: `{"items":[{` +
-						`"status":"OK"}]}`}}, SkipToken: true},
-				{Point: &common.DataPoint{UniqId: uniqID, Attr: "ack",
-					ValOneof: &common.DataPoint_StrVal{StrVal: "OK"}},
-					SkipToken: true},
-			}},
-		{"lora/gateway/" + uniqID + "/event/exec",
+		{
+			"lora/gateway/" + uniqID + "/event/up", &gw.UplinkFrame{
+				RxInfo: &gw.UplinkRXInfo{Rssi: -74},
+			}, []*message.ValidatorIn{
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_gateway",
+						ValOneof: &common.DataPoint_StrVal{
+							StrVal: `{"rxInfo":{"rssi":-74}}`,
+						},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "lora_rssi",
+						ValOneof: &common.DataPoint_IntVal{IntVal: -74},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "channel",
+						ValOneof: &common.DataPoint_IntVal{IntVal: 0},
+					}, SkipToken: true,
+				},
+			},
+		},
+		{
+			"lora/gateway/" + uniqID + "/event/stats", &gw.GatewayStats{
+				RxPacketsReceivedOk: 2,
+			}, []*message.ValidatorIn{
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_gateway",
+						ValOneof: &common.DataPoint_StrVal{
+							StrVal: `{"rxPacketsReceivedOK":2}`,
+						},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "rx_received_valid",
+						ValOneof: &common.DataPoint_IntVal{IntVal: 2},
+					}, SkipToken: true,
+				},
+			},
+		},
+		{
+			"lora/gateway/" + uniqID + "/event/ack", &gw.DownlinkTXAck{
+				Items: []*gw.DownlinkTXAckItem{{Status: gw.TxAckStatus_OK}},
+			}, []*message.ValidatorIn{
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_gateway",
+						ValOneof: &common.DataPoint_StrVal{
+							StrVal: `{"items":[{"status":"OK"}]}`,
+						},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "ack",
+						ValOneof: &common.DataPoint_StrVal{StrVal: "OK"},
+					}, SkipToken: true,
+				},
+			},
+		},
+		{
+			"lora/gateway/" + uniqID + "/event/exec",
 			&gw.GatewayCommandExecResponse{Stdout: []byte("STDOUT")},
 			[]*message.ValidatorIn{
-				{Point: &common.DataPoint{UniqId: uniqID, Attr: "raw_gateway",
-					ValOneof: &common.DataPoint_StrVal{
-						StrVal: `{"stdout":"U1RET1VU"}`}}, SkipToken: true},
-				{Point: &common.DataPoint{UniqId: uniqID, Attr: "exec_stdout",
-					ValOneof: &common.DataPoint_StrVal{StrVal: "STDOUT"}},
-					SkipToken: true},
-			}},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_gateway",
+						ValOneof: &common.DataPoint_StrVal{
+							StrVal: `{"stdout":"U1RET1VU"}`,
+						},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "exec_stdout",
+						ValOneof: &common.DataPoint_StrVal{StrVal: "STDOUT"},
+					}, SkipToken: true,
+				},
+			},
+		},
+		{
+			"lora/gateway/" + uniqID + "/state/conn", &gw.ConnState{
+				State: gw.ConnState_ONLINE,
+			}, []*message.ValidatorIn{
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_gateway",
+						ValOneof: &common.DataPoint_StrVal{
+							StrVal: `{"state":"ONLINE"}`,
+						},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "conn",
+						ValOneof: &common.DataPoint_StrVal{StrVal: "ONLINE"},
+					}, SkipToken: true,
+				},
+			},
+		},
 	}
 
 	for _, test := range tests {
@@ -132,66 +197,141 @@ func TestDecodeDevices(t *testing.T) {
 		resVIn   []*message.ValidatorIn
 		resPIn   *message.DecoderIn
 	}{
-		{"lora/application/1/device/" + uniqID + "/event/up", &as.UplinkEvent{
-			Data: bData}, []*message.ValidatorIn{
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "raw_device",
-				ValOneof: &common.DataPoint_StrVal{StrVal: fmt.Sprintf(
-					`{"data":"%s"}`, b64Data)}}, SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "raw_data",
-				ValOneof: &common.DataPoint_StrVal{
-					StrVal: hex.EncodeToString(bData)}}, SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "adr",
-				ValOneof: &common.DataPoint_BoolVal{BoolVal: false}},
-				SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "data_rate",
-				ValOneof: &common.DataPoint_IntVal{IntVal: 0}},
-				SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "confirmed",
-				ValOneof: &common.DataPoint_BoolVal{BoolVal: false}},
-				SkipToken: true},
-		}, &message.DecoderIn{UniqId: uniqID, Data: bData}},
-		{"lora/application/2/device/" + uniqID + "/event/join", &as.JoinEvent{
-			RxInfo: []*gw.UplinkRXInfo{{}}, Dr: 3}, []*message.ValidatorIn{
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "raw_device",
-				ValOneof: &common.DataPoint_StrVal{
-					StrVal: `{"rxInfo":[{}],"dr":3}`}}, SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "join",
-				ValOneof: &common.DataPoint_BoolVal{BoolVal: true}},
-				SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "channel",
-				ValOneof: &common.DataPoint_IntVal{IntVal: 0}},
-				SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "data_rate",
-				ValOneof: &common.DataPoint_IntVal{IntVal: 3}},
-				SkipToken: true},
-		}, nil},
-		{"lora/application/3/device/" + uniqID + "/event/ack", &as.AckEvent{
-			Acknowledged: true}, []*message.ValidatorIn{
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "raw_device",
-				ValOneof: &common.DataPoint_StrVal{
-					StrVal: `{"acknowledged":true}`}}, SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "ack",
-				ValOneof: &common.DataPoint_StrVal{StrVal: "OK"}},
-				SkipToken: true},
-		}, nil},
-		{"lora/application/4/device/" + uniqID + "/event/error", &as.ErrorEvent{
-			Type: as.ErrorType_OTAA}, []*message.ValidatorIn{
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "raw_device",
-				ValOneof: &common.DataPoint_StrVal{
-					StrVal: `{"type":"OTAA"}`}}, SkipToken: true},
-			{Point: &common.DataPoint{UniqId: uniqID, Attr: "error_type",
-				ValOneof: &common.DataPoint_StrVal{StrVal: "OTAA"}},
-				SkipToken: true},
-		}, nil},
-		{"lora/application/5/device/" + uniqID + "/event/txack",
-			&as.TxAckEvent{}, []*message.ValidatorIn{
-				{Point: &common.DataPoint{UniqId: uniqID, Attr: "raw_device",
-					ValOneof: &common.DataPoint_StrVal{StrVal: `{}`}},
-					SkipToken: true},
-				{Point: &common.DataPoint{UniqId: uniqID,
-					Attr: "ack_gateway_tx", ValOneof: &common.DataPoint_BoolVal{
-						BoolVal: true}}, SkipToken: true},
-			}, nil},
+		{
+			"lora/application/1/device/" + uniqID + "/event/up",
+			&as.UplinkEvent{Data: bData},
+			[]*message.ValidatorIn{
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_device",
+						ValOneof: &common.DataPoint_StrVal{
+							StrVal: fmt.Sprintf(`{"data":"%s"}`, b64Data),
+						},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_data",
+						ValOneof: &common.DataPoint_StrVal{
+							StrVal: hex.EncodeToString(bData),
+						},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "adr",
+						ValOneof: &common.DataPoint_BoolVal{BoolVal: false},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "data_rate",
+						ValOneof: &common.DataPoint_IntVal{IntVal: 0},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "confirmed",
+						ValOneof: &common.DataPoint_BoolVal{BoolVal: false},
+					}, SkipToken: true,
+				},
+			},
+			&message.DecoderIn{UniqId: uniqID, Data: bData},
+		},
+		{
+			"lora/application/2/device/" + uniqID + "/event/join",
+			&as.JoinEvent{RxInfo: []*gw.UplinkRXInfo{{}}, Dr: 3},
+			[]*message.ValidatorIn{
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_device",
+						ValOneof: &common.DataPoint_StrVal{
+							StrVal: `{"rxInfo":[{}],"dr":3}`,
+						},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "join",
+						ValOneof: &common.DataPoint_BoolVal{BoolVal: true},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "channel",
+						ValOneof: &common.DataPoint_IntVal{IntVal: 0},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "data_rate",
+						ValOneof: &common.DataPoint_IntVal{IntVal: 3},
+					}, SkipToken: true,
+				},
+			},
+			nil,
+		},
+		{
+			"lora/application/3/device/" + uniqID + "/event/ack", &as.AckEvent{
+				Acknowledged: true,
+			}, []*message.ValidatorIn{
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_device",
+						ValOneof: &common.DataPoint_StrVal{
+							StrVal: `{"acknowledged":true}`,
+						},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "ack",
+						ValOneof: &common.DataPoint_StrVal{StrVal: "OK"},
+					}, SkipToken: true,
+				},
+			}, nil,
+		},
+		{
+			"lora/application/4/device/" + uniqID + "/event/error",
+			&as.ErrorEvent{Type: as.ErrorType_OTAA},
+			[]*message.ValidatorIn{
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_device",
+						ValOneof: &common.DataPoint_StrVal{
+							StrVal: `{"type":"OTAA"}`,
+						},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "error_type",
+						ValOneof: &common.DataPoint_StrVal{StrVal: "OTAA"},
+					}, SkipToken: true,
+				},
+			},
+			nil,
+		},
+		{
+			"lora/application/5/device/" + uniqID + "/event/txack",
+			&as.TxAckEvent{},
+			[]*message.ValidatorIn{
+				{
+					Point: &common.DataPoint{
+						UniqId: uniqID, Attr: "raw_device",
+						ValOneof: &common.DataPoint_StrVal{StrVal: `{}`},
+					}, SkipToken: true,
+				},
+				{
+					Point: &common.DataPoint{
+						UniqId:   uniqID,
+						Attr:     "ack_gateway_tx",
+						ValOneof: &common.DataPoint_BoolVal{BoolVal: true},
+					}, SkipToken: true,
+				},
+			},
+			nil,
+		},
 	}
 
 	for _, test := range tests {
@@ -278,11 +418,17 @@ func TestDecodeGatewaysDevicesError(t *testing.T) {
 		inpPayl  []byte
 	}{
 		// Bad topic.
-		{"lora", nil},
+		{
+			"lora", nil,
+		},
 		// Bad payload.
-		{"lora/gateway/" + uniqID + "/event/up", []byte("ing-aaa")},
-		{"lora/application/1/device/" + uniqID + "/event/up",
-			[]byte("ing-aaa")},
+		{
+			"lora/gateway/" + uniqID + "/event/up", []byte("ing-aaa"),
+		},
+		{
+			"lora/application/1/device/" + uniqID + "/event/up",
+			[]byte("ing-aaa"),
+		},
 	}
 
 	for _, test := range tests {
