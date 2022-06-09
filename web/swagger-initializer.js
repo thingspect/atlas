ea266764ef1b3a1e1b83bb6f6c96d6ab55e8aa0e
Brian Conway <bconway@rcesoftware.com>
2022-06-09 13:23:38 -0500

    Update swagger-ui-dist to v4.12.0

---
+++ web/swagger-initializer.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "atlas.swagger.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout",
+    defaultModelRendering: "model",
+    defaultModelExpandDepth: 10,
+    displayRequestDuration: true
+  });
+
+  //</editor-fold>
+};
